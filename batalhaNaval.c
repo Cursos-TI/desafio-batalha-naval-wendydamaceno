dd8c743997753d19f2ce46fbc453d16f2c3e1637
Wendy Santos <200622375+wendydamaceno@users.noreply.github.com>
2025-04-04 17:51:54 -0300

    Update batalhaNaval.c
    Batalha Naval - Desafio Nível Novato.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,5 +2,45 @@
 
 int main(){
 
+    //Desafio Nível Novato
+    printf("\n***BATALHA NAVAL***\n");
+
+    int tabuleiro[10][10];
+    char letras[] = {'A','B','C','D','E','F','G','H','I','J'};
+
+    //Inicializando com 0 (água)
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            tabuleiro[i][j] = 0;
+        }
+    }
+    
+    // Posicionar navio horizontal (linha B, colunas 3 a 5)
+    tabuleiro[1][2] = 3;
+    tabuleiro[1][3] = 3;
+    tabuleiro[1][4] = 3;
+
+    // Posicionar navio vertical (coluna F, linhas E a G)
+    tabuleiro[4][5] = 3;
+    tabuleiro[5][5] = 3;
+    tabuleiro[6][5] = 3;
+
+     // Cabeçalho com colunas 1–10
+     printf("    ");
+     for (int col = 1; col <= 10; col++) {
+         printf("%-3d", col);
+     }
+     printf("\n");
+ 
+     // Impressão do tabuleiro com linhas A–J
+     for (int i = 0; i < 10; i++) {
+         printf(" %c |", letras[i]); // Mostra A–J
+         for (int j = 0; j < 10; j++) {
+             printf(" %-2d", tabuleiro[i][j]);
+         }
+         printf("\n");
+     }
+    
+
     return 0;
 }
